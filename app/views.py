1ace19e2ab81ac043494c5bb49106ef6e8970fed
Maria Kerechanina <maria15@mail.ru>
2018-11-13 00:14:15 +0300

    HW4. tests

--- app/views.py
+++ app/views.py
@@ -10,24 +10,24 @@ def index(name="world"):
 def user_profile(username):
   return 'User %s' % username
 
-@app.route('/chats/')
-def chats():
-  user_chats = ['Football', '204', 'Cyclades']
-  user_chs = json.dumps(user_chats)
+@app.route('/list_chats/')
+def list_chats():
+  user_chats_array = ['Football', '204', 'Cyclades']
+  user_chats = json.dumps(user_chats_array)
   response = app.response_class(
-      response = user_chs,
+      response = user_chats,
       mimetype='application/json',
       status=200
 
   )
   return response
 
-@app.route('/contacts/')
-def contacts():
-  user_contacts = ['Vitek', 'Theodore', 'Nikitos', 'Vovan']
-  user_cs = json.dumps(user_contacts)
+@app.route('/list_contacts/')
+def list_contacts():
+  user_contacts_array = ['Vitek', 'Theodore', 'Nikitos', 'Vovan']
+  user_contacts = json.dumps(user_contacts_array)
   response = app.response_class(
-      response = user_cs,
+      response = user_contacts,
       mimetype='application/json',
       status=200
 
@@ -46,25 +46,8 @@ def form():
 </body></html>
 """
     else:
-      print(5)
-      abort(404) 
-
-
-@app.route('/chats/add/', methods=['GET', 'POST'])
-def new_chat():
-    if request.method =='GET':
-      return """<html><head></head><body>
-      <form method="POST" action="/form/">
-         <input name="chat_name" >
-         <input type="submit" >
-      </form>
-</body></html>
-"""
-    else:
-      #request.form['chat_name']
-      return 
-      print(6)
-      abort(404) 
+      rv = jsonify(request.form)
+      return rv
 
 
 @app.route('/contacts/add/', methods=['GET', 'POST'])
@@ -78,10 +61,60 @@ def new_contact():
 </body></html>
 """
     else:
-      #request.form['chat_name']
       return 
-      print(6)
-      abort(404) 
+
+
+
+
+@app.route('/chats/create_pers_chat/', methods=['GET', 'POST'])
+def create_pers_chat():
+    if request.method =='GET':
+      return """<html><head></head><body>
+      <form method="POST" action="/chats/create_pers_chat/">
+         <input name="friend_name" >
+         <input type="submit" >
+      </form>
+</body></html>
+"""
+    else:
+      rv = jsonify(request.form)
+      return rv
+      return 
+
+@app.route('/chats/create_group_chat/', methods=['GET', 'POST'])
+def create_group_chat():
+    if request.method =='GET':
+      return """<html><head></head><body>
+      <form method="POST" action="/form/">
+         <input name="chat_name" >
+         <input type="submit" >
+      </form>
+</body></html>
+"""
+    else:
+      rv = jsonify(request.form)
+      return rv
+      return 
+
+#@app.route('/chats/add_members_to_group_chat/', methods=['GET', 'POST'])
+#def add_members_to_group_chat():
+
+#@app.route('/chats/leave_group_chat/', methods=['GET', 'POST'])
+#def leave_group_chat():
+
+#@app.route('/chats/send_message/', methods=['GET', 'POST'])
+#def send_message():
+
+#@app.route('/chats/read_message/', methods=['GET', 'POST'])
+#def read_message():
+
+#@app.route('/chats/upload_file/', methods=['GET', 'POST'])
+#def upload_file():
+
+
+
+
+#12-11-18
 
 #@jsonrpc.method( 'api.upload_file' )
 #def upload_file( Bucket=config.S3_BUCKET_NAME, Key=filename, Body=b64content ):
--- tests/app_tests.py
+++ tests/app_tests.py
@@ -5,9 +5,37 @@ from app import app
 class AppTest(unittest.TestCase):
 
     def setUp(self): 
-        self.app = app.test_client()       
+        self.app = app.test_client()
 
+    def test_index(self):
+        rv = self.app.get('/')
+        self.assertEqual(b'Hello, world!', rv.data)
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual("text/html", rv.mimetype)   
 
+    def test_list_chats(self):
+        rv = self.app.get('/list_chats/')
+        self.assertEqual(b'["Football", "204", "Cyclades"]', rv.data)
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual("application/json", rv.mimetype)  
+
+    def test_list_contacts(self):
+        rv = self.app.get('/list_contacts/')
+        self.assertEqual(b'["Vitek", "Theodore", "Nikitos", "Vovan"]', rv.data)
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual("application/json", rv.mimetype)     
+
+    def test_create_pers_chat_post(self):
+        rv = self.app.post('/chats/create_pers_chat/', data = {"friend_name": "Marina"})
+        self.assertEqual(b'{"friend_name":"Marina"}\n', rv.data)
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual("application/json", rv.mimetype) 
+
+    def test_create_group_chat_get(self):
+        rv = self.app.post('/chats/create_pers_chat/', data = {"chat_name":"Helloween"})
+        self.assertEqual(b'{"chat_name":"Helloween"}\n', rv.data)
+        self.assertEqual(200, rv.status_code)
+        self.assertEqual("application/json", rv.mimetype)  
 
 if __name__ == "__main__":
      unittest.main()
