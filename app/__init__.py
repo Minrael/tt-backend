ec93c051a497e3e831b06c041950f43c1e8c25e9
Maria Kerechanina <maria15@mail.ru>
2018-11-12 17:20:16 +0300

    homework4

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_pyfile('../config.py')
+app.config.from_pyfile('../instance/config.py')
+
 
 from .views import *
--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-from flask import request, abort, jsonify
-
+from flask import request, abort, jsonify, json
 from app import app
 
 @app.route('/')
@@ -7,18 +6,44 @@ from app import app
 def index(name="world"):
     return "Hello, {}!".format( name )
 
-@app.route('/form/', methods=['GET', 'POST'])
-def form():
-    if request.method =='GET':
-        return """<html><head></head><body>
-        <form method="POST" action="/form/">
-            <input name="first_name" >
-            <input name="last_name" >
-            <input type="submit" >
-        </form>
-        </body></html>"""
-    else:
-        rv = jsonify( request.form )
-        return rv
-        #print( request.form )
-        #abort(404)
+#@app.route('/form/', methods=['GET', 'POST'])
+#def form():
+#    if request.method =='GET':
+#        return """<html>
+#			<head></head>
+#			<body>
+#        		</body>
+#		</html>"""
+ #   else:
+#        response = jsonify( )
+#        return response
+
+
+
+#@app.route('/user/<username>')
+#  def user_profile(username):
+#    return 'User %s' % username
+
+@app.route('/chats/')
+def chats():
+  user_chats = jsonify('Football', '204', 'Cyclades')
+  return user_chats
+
+@app.route('/contacts/')
+def contacts():
+  user_contacts = jsonify('Vitek', 'Theodore', 'Nikitos', 'Vovan')
+  return user_contacts
+
+#@app.route('/contacts/add/')
+#def new_contact():
+#  user_contacts = jsonify('Vitek', 'Theodore', 'Nikitos', 'Vovan')
+#  return user_contacts
+
+#@app.route('/chats/create/', methods=['GET', 'POST'])
+#def new_chat():
+
+#return 
+
+
+
+
